f89ef5b036228eb479a89e6429707da26f1d0bf8
psainzpc <psainzpc@hotmail.com>
2024-11-24 10:03:10 +0100

    columna satisfaction

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from src import soporte as sp
 from src import variables as va
-from src import soporte_queries_creacion_BDD as query
-from src import bdd_soporte as bdd
+#from src import soporte_queries_creacion_BDD as query
+#from src import bdd_soporte as bdd
 import pandas as pd
+import numpy as np
 
 df = pd.read_csv("data/hr_raw_data_final.csv", index_col=0)
 
@@ -79,8 +80,13 @@ print("reemplazamos los no data por otro concepto")
 sp.replace_nodata(df,"educationfield", "other")
 sp.replace_nodata(df,"department", "general")
 
+print("creamos columna con satisfacción")
+df["satisfaction"] = np.where(df["jobsatisfaction"] < 3, "not satisfied", "satisfied")
+
 print("cambiamos el orden de las columnas y el nombre")
 df=df[va.new_order]
-df.columns = ['employee_number','attrition','job_level', 'job_role','department','monthly_income','salary','percent_salary_hike','training_times_last_year','stock_option_level','hourly_rate','daily_rate','monthly_rate','standard_hours','overtime','business_travel','distance_from_home','remote_work','environment_satisfaction','job_involvement','job_satisfaction','performance_rating','relationship_satisfaction','work_life_balance','date_birth','age','gender','marital_status','education', 'education_field','num_companies_worked','total_working_years','years_at_company','years_since_last_promotion','years_with_curr_manager']
+df.columns = ['employee_number','attrition','job_level', 'job_role','department','monthly_income','salary','percent_salary_hike','training_times_last_year','stock_option_level','hourly_rate','daily_rate','monthly_rate','standard_hours','overtime','business_travel','distance_from_home','remote_work','environment_satisfaction','job_involvement','job_satisfaction','satisfaction', 'performance_rating','relationship_satisfaction','work_life_balance','date_birth','age','gender','marital_status','education', 'education_field','num_companies_worked','total_working_years','years_at_company','years_since_last_promotion','years_with_curr_manager']
 
+
+print("guardar csv con cambios hechos")
 df.to_csv("data/hr_raw_data_definitivo.csv")
